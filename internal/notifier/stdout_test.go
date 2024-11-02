112919b57a7e7f17a9f8fa85242cecc4f63e07b1
Ivan Godwin <ivan.godwin@gmail.com>
2024-11-02 12:55:21 -0700

    Move stdout reset steps to function

--- internal/notifier/stdout_test.go
+++ internal/notifier/stdout_test.go
@@ -14,16 +14,16 @@ import (
 
 const expectedNotificationMessage = "this is a test notification message"
 
-var _ = Describe("StdoutNotifier", func() {
-	var (
-		stdoutNotifier *notifier.StdoutNotifier
-		cfg            config.StdoutConfig
-		buffer         *bytes.Buffer
-		reader         *os.File
-		writer         *os.File
-		originalStdout *os.File
-	)
+var (
+	stdoutNotifier *notifier.StdoutNotifier
+	cfg            config.StdoutConfig
+	buffer         *bytes.Buffer
+	reader         *os.File
+	writer         *os.File
+	originalStdout *os.File
+)
 
+var _ = Describe("StdoutNotifier", func() {
 	BeforeEach(func() {
 		cfg = config.StdoutConfig{}
 		stdoutNotifier, _ = notifier.NewStdoutNotifier(cfg)
@@ -38,9 +38,7 @@ var _ = Describe("StdoutNotifier", func() {
 	It("should output the correct message to stdout", func() {
 		testNotification := notifier.Notification{Message: expectedNotificationMessage}
 		Expect(stdoutNotifier.Send(testNotification)).To(Succeed())
-
-		Expect(writer.Close()).To(Succeed())
-		os.Stdout = originalStdout
+		resetStdout()
 
 		_, _ = buffer.ReadFrom(reader)
 		Expect(buffer.String()).To(ContainSubstring(expectedNotificationMessage))
@@ -49,11 +47,14 @@ var _ = Describe("StdoutNotifier", func() {
 	It("should not output anything if message is empty", func() {
 		testNotification := notifier.Notification{}
 		Expect(stdoutNotifier.Send(testNotification)).To(Succeed())
-
-		Expect(writer.Close()).To(Succeed())
-		os.Stdout = originalStdout
+		resetStdout()
 
 		_, _ = buffer.ReadFrom(reader)
 		Expect(strings.TrimSpace(buffer.String())).To(BeEmpty())
 	})
 })
+
+func resetStdout() {
+	Expect(writer.Close()).To(Succeed())
+	os.Stdout = originalStdout
+}
